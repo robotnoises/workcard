4db2bdce17a26ac882d0cf18bf554992c48d8590
DAVID <DAVENICH@GMAIL.COM>
2021-01-06 14:35:57 -0600

    chore: add faunadb javascript driver

---
+++ functions/card-get.ts
@@ -0,0 +1,12 @@
+import { faunaDb, q } from './lib';
+
+const id = '287001482713104896'; // todo: temp
+
+exports.handler = async function (event, context) {
+  const response = await faunaDb.query(q.Get(q.Ref(`workcard/cards/${id}`)));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: response }),
+  };
+};
