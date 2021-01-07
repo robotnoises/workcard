9802b6d4662fa13c5c5f2f79ec64e5da70fe898f
David Nichols <davenich@gmail.com>
2021-01-06 20:30:57 -0600

    chore: add create function

---
+++ functions/card-create.js
@@ -0,0 +1,30 @@
+const faunadb = require('faunadb');
+const q = faunadb.query
+
+exports.handler = async function (event, context) {
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SECRET,
+  });
+
+  const fakeData = {
+    firstName: 'Foo',
+    lastName: 'Barson',
+    title: 'Guy',
+    employer: 'Company A',
+    summary: 'Cool, chill as hell',
+    skills: ['none'],
+  };
+
+  let response;
+
+  try {
+    response = await client.query(q.Create(q.Collection('cards'), fakeData));
+  } catch (ex) {
+    console.error({ ex });
+  }
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: response.data }),
+  };
+};
