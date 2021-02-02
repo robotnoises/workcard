f4294f8b3bc96e9a12bf7d4e7eb963347a799ca1
DAVID <DAVENICH@GMAIL.COM>
2021-02-02 13:58:01 -0600

    chore: return entire response from contentful

--- functions/card-get.js
+++ functions/card-get.js
@@ -21,6 +21,6 @@ exports.handler = async function (event, context) {
   
   return {
     statusCode: 200,
-    body: JSON.stringify({ data: response.data }),
+    body: JSON.stringify({ data: response }),
   };
 };
