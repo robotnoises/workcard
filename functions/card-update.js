72cbcaf3aacbdce62c7b4f413324ae48480f3065
DAVID <DAVENICH@GMAIL.COM>
2021-02-02 14:20:11 -0600

    chore: add contentful-management sdk

---
+++ functions/card-update.js
@@ -0,0 +1,25 @@
+const contentful = require('contentful');
+
+const space = process.env.CONTENTFUL_SPACE_ID;
+const accessToken = process.env.CONTENTFUL_API_TOKEN;
+
+const id = '5C1xUsOd7UGgSdwAO4knVg'; // todo: temp
+
+exports.handler = async function (event, context) {
+  const client = contentful.createClient({
+    space,
+    accessToken,
+  })
+
+  // let response;
+
+  // try {
+  //   response = await client.getEntry(id);
+  // } catch (ex) {
+  //   console.error({ ex });
+  // }
+  
+  return {
+    statusCode: 200,
+  };
+};
